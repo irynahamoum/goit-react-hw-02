bb3ac60c77c838469f8c0a98d92e916f9cb127cf
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-03-14 18:53:15 +0100

    step-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import Description from './components/Description/Description';
 import description from './assets/Description.json';
 import Options from './components/Options/Options';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Feedback from './components/Feedback/Feedback';
 import Notification from './components/Notification/Notification';
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = JSON.parse(window.localStorage.getItem('saved-feedback'));
+    console.log(savedFeedback);
+    const originalFeedback = { good: 0, neutral: 0, bad: 0 };
+    return savedFeedback !== null ? savedFeedback : originalFeedback;
+  });
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   const positive = totalFeedback > 0 ? Math.round(((feedback.good + feedback.neutral) / totalFeedback) * 100) : 0;
+  useEffect(() => {
+    window.localStorage.setItem('saved-feedback', JSON.stringify(feedback));
+  }, [feedback]);
   const updateFeedback = (feedbackType) => {
     setFeedback((prevFeedback) => ({
       ...prevFeedback,
